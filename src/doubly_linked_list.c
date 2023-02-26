b9e3ac662461cda9b213a702c54d5380fe06552e
spaceymonk <b.kaan.ozkan@gmail.com>
2023-02-26 11:20:29 +0300

    feat(DLL_impl): Add pop_back and print methods

--- src/doubly_linked_list.c
+++ src/doubly_linked_list.c
@@ -13,6 +13,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 dll_t *dll_new()
 {
@@ -32,6 +33,22 @@ int dll_free(dll_t *dll)
     }
     fprintf(stderr, "dll_free: dll is not empty\n");
     return 1;
+}
+
+void dll_print(dll_t *self, FILE *fd, char *(*to_string)(void *))
+{
+    fwrite("[", 1, 2, fd);
+    for (dll_node_t *node = self->head; node != NULL; node = node->next)
+    {
+        char *str = to_string(node->data);
+        fwrite(str, 1, strlen(str), fd);
+        free(str);
+        if (node->next)
+        {
+            fwrite(", ", 1, 3, fd);
+        }
+    }
+    fwrite("]\n", 1, 3, fd);
 }
 
 void *dll_push_back(dll_t *self, void *data)
@@ -69,5 +86,51 @@ void *dll_push_front(dll_t *self, void *data)
         self->tail = node;
     }
     self->size++;
+    return data;
+}
+
+void *dll_pop_back(dll_t *self)
+{
+    if (self->tail == NULL)
+    {
+        fprintf(stderr, "dll_pop_back: List is empty");
+        return NULL;
+    }
+    dll_node_t *node = self->tail;
+    void *data = node->data;
+    self->tail = self->tail->prev;
+    if (self->tail)
+    {
+        self->tail->next = NULL;
+    }
+    else
+    {
+        self->head = NULL;
+    }
+    self->size--;
+    free(node);
+    return data;
+}
+
+void *dll_pop_front(dll_t *self)
+{
+    if (self->head == NULL)
+    {
+        fprintf(stderr, "dll_pop_front: List is empty");
+        return NULL;
+    }
+    dll_node_t *node = self->head;
+    void *data = node->data;
+    self->head = self->head->next;
+    if (self->head)
+    {
+        self->head->prev = NULL;
+    }
+    else
+    {
+        self->tail = NULL;
+    }
+    self->size--;
+    free(node);
     return data;
 }
