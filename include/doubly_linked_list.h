3a272746a4914ec48952a24723d12454fd3048ee
spaceymonk <b.kaan.ozkan@gmail.com>
2023-02-26 12:59:34 +0300

    feat(dll): Add interfaces of sort, search, reverse

--- include/doubly_linked_list.h
+++ include/doubly_linked_list.h
@@ -174,7 +174,7 @@ void *dll_set(dll_t *self, void *data, size_t index);
 
 /**
  * @brief Clears the list and frees all nodes by
- *      calling the free function on the data
+ *       calling the free function on the data
  *
  * @param self          The list to clear
  * @param free_handler  The function to free the data. This
@@ -186,5 +186,83 @@ void *dll_set(dll_t *self, void *data, size_t index);
  * @return void
  */
 void dll_clear(dll_t *self, void (*free_handler)(void *));
+
+/**
+ * @brief Search for the given data in the list
+ *       and returns the node that contains it
+ *
+ * @param self  The list to search in
+ * @param data  The data to search for
+ * @param cmp   The comparison function to compare the data,
+ *             if this param is NULL, the data will be compared
+ *             using pointer equality.
+ *
+ * @note The comparasion function should return 0 if the
+ *      data is equal.
+ *
+ * @return The node that contains the data, NULL if not found
+ */
+dll_node_t *dll_search(dll_t *self, void *data, int (*cmp)(void *, void *));
+
+/**
+ * @brief Binary search for the given data in the list
+ *      and returns the node that contains it
+ *
+ * @param self  The list to search in
+ * @param data  The data to search for
+ * @param cmp   The comparison function to compare the data
+ *
+ * @attention The list must be sorted for this function to work.
+ *
+ * @note The comparasion function should return 0 if the
+ *      data is equal, negative if the first data is less
+ *      than the second, and positive otherwise.
+ *
+ * @return The node that contains the data, NULL if not found
+ */
+dll_node_t *dll_binary_search(dll_t *self, void *data, int (*cmp)(void *, void *));
+
+/**
+ * @brief Sorts the list in place using the given comparision
+ *       function and returns the sorted list
+ *
+ * @param self  The list to sort
+ * @param cmp   The comparison function to compare the data
+ *
+ * @note The comparasion function should return 0 if the
+ *      data is equal, negative if the first data is less
+ *      than the second, and positive otherwise.
+ *
+ * @note This function uses the quick sort algorithm.
+ *
+ * @return `self` as a sorted list
+ */
+dll_t *dll_sort(dll_t *self, int (*cmp)(void *, void *));
+
+/**
+ * @brief Stable sorts the list in place using the given
+ *       comparision function and returns the sorted list
+ *
+ * @param self  The list to sort
+ * @param cmp   The comparison function to compare the data
+ *
+ * @note The comparasion function should return 0 if the
+ *     data is equal, negative if the first data is less
+ *    than the second, and positive otherwise.
+ *
+ * @note This function uses the merge sort algorithm.
+ *
+ * @return `self` as a sorted list
+ */
+dll_t *dll_stable_sort(dll_t *self, int (*cmp)(void *, void *));
+
+/**
+ * @brief Reverses the list in place and returns it
+ * 
+ * @param self  The list to reverse
+ * 
+ * @return `self` as a reversed list
+ */
+dll_t *dll_reverse(dll_t *self);
 
 #endif
