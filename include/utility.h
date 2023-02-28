7aaa9165ac0b20e7465b7a959f0e52fce32a17b1
spaceymonk <b.kaan.ozkan@gmail.com>
2023-02-28 20:59:36 +0300

    feat: add utility function swap

---
+++ include/utility.h
@@ -0,0 +1,23 @@
+/**
+ * @file utility.h
+ * @author Berktug K. Ozkan (@spaceymonk)
+ * @brief Utility functions declarations
+ * @version 0.1
+ * @date 2023-02-28
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef UTILITY_H
+#define UTILITY_H
+
+/**
+ * @brief Swaps the values of two pointers
+ *
+ * @param a Pointer to the first value
+ * @param b Pointer to the second value
+ */
+void swap(void *a, void *b);
+
+#endif
