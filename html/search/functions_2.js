da6070777322e8c58dba569e3134cf9d9020117e
spaceymonk <b.kaan.ozkan@gmail.com>
2023-03-02 20:04:05 +0300

    infra: move documents into docs folder

--- html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['swap_90',['swap',['../utility_8h.html#a65fb3038fc2b214b1e80142022812fcf',1,'swap(void **a, void **b):&#160;utiliy.c'],['../utiliy_8c.html#a65fb3038fc2b214b1e80142022812fcf',1,'swap(void **a, void **b):&#160;utiliy.c']]]
-];
